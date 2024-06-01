372bd3787b3071337c2d39fe75475d735594fc15
kaydurgu <mzhanbolot@gmail.com>
2024-06-01 14:00:05 +0300

    urls for Sensors app added, IsInGroup custom permission added

--- air_quality/views.py
+++ air_quality/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import permissions
+
 
 # Create your views here.
+
+class IsInGroup(permissions.BasePermission):
+    group_name = None
+    def has_permission(self, request, view):
+        if request.user and request.user.is_authenticated:
+            return request.user.groups.filter(name=self.group_name).exists()
+        return False
+
+class IsAdminUser(IsInGroup):
+    group_name = 'Admin'
+class IsWorkerUser(IsInGroup):
+    group_name = 'Worker'
+
